89d19727887aa268f9699100b3114623b7c386ad
uma <chiderovauma@gmail.com>
2023-12-05 16:05:30 +0600

    Refactored the "display_hero_info()" function.

--- steps/helpers/display_functions.py
+++ steps/helpers/display_functions.py
@@ -5,18 +5,14 @@ from helpers.variables import HERO_WAS_INJURED_EVENT, \
     ROBOT_FINISHED_EVENT
 
 def display_hero_info(event: str, args=None) -> None or str:
-    if args is not None:
-        if event == HERO_WAS_INJURED_EVENT:
-            display_character_info(args,
-                                   character_messages_dict=hero_info_messages,
-                                   consts=[HERO_WAS_INJURED_EVENT, HERO_FINISHED_EVENT])
-        elif event == HERO_FINISHED_EVENT:
-            hero_hp = args
-            return hero_info_messages.get(HERO_FINISHED_EVENT).format(hero_hp)
-        else:
-            print(hero_info_messages.get(event).format(args))
+    if event == HERO_WAS_INJURED_EVENT:
+        display_character_info(args,
+                               character_messages_dict=hero_info_messages,
+                               consts=[HERO_WAS_INJURED_EVENT, HERO_FINISHED_EVENT])
+    elif event == HERO_FINISHED_EVENT:
+        return hero_info_messages.get(HERO_FINISHED_EVENT).format(args)
     else:
-        print(hero_info_messages.get(event))
+        print(hero_info_messages.get(event).format(args) if args is not None else hero_info_messages.get(event))
 
 def display_robot_info(event: str, args=None) -> None or str:
     if event == ROBOT_WAS_INJURED_EVENT:
