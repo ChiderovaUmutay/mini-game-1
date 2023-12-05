ad7d4b412964b74a00ec8a673335fa12b0771ff0
uma <chiderovauma@gmail.com>
2023-12-05 17:55:30 +0600

    Translated all Russian text into English.

--- steps/helpers/info_messages.py
+++ steps/helpers/info_messages.py
@@ -1,27 +1,27 @@
 hero_info_messages: dict = {
-    "hero_took_damage": "HIT! HIT! HIT!\nГерой получил {} ед. урона!\n",
-    "hero_health_info": "Остаток здоровья у героя составляет {} ед.\n",
-    "hero_missed": "Герой промазал!\n",
-    "hero_misses_turn": "Герой пропускает ход!\n",
-    "hero_attacks": "Герой атакует!\n",
-    "hero_defends_himself": "Герой защищается и активирует защитное поле, сила которого равна {}!\n",
-    "hero_deactivate_protected_field": "Герой снял защитное поле!\n",
-    "hero_repelled_attack": "Герой отразил атаку защитным полем!\n",
-    "hero_injected_adrenaline": "Герой вколол адреналин!\n",
-    "adrenaline_qty_info": "Количество шприца с адреналином в рюкзаке равно {}!\n",
-    "adrenaline_ended": "Шприцы с адреналином закончились!\nГерой делает повторный ход.\n"
+    "hero_took_damage": "HIT! HIT! HIT!\nThe hero received {} units damage!\n",
+    "hero_health_info": "The hero's remaining health is {} units.\n",
+    "hero_missed": "The hero missed!\n",
+    "hero_misses_turn": "The hero misses a turn!\n",
+    "hero_attacks": "Hero attacks!\n",
+    "hero_defends_himself": "The hero defends himself and activates a protective field, the strength of which is equal to {}!\n",
+    "hero_deactivate_protected_field": "The hero removed the protective field!\n",
+    "hero_repelled_attack": "The hero repelled the attack with a protective field!\n",
+    "hero_injected_adrenaline": "The hero injected adrenaline!\n",
+    "adrenaline_qty_info": "The number of syringes with adrenaline in the backpack is equal to {}!\n",
+    "adrenaline_ended": "The syringes with adrenaline have run out!\nThe hero makes a second move.\n"
 }
 
 robot_info_messages: dict = {
-    "robot_took_damage": "HIT! HIT! HIT!\nРобот получил {} ед. урона!\n",
-    "robot_health_info": "Остаток здоровья у робота составляет {} ед.\n",
-    "robot_missed": "Робот промазал!\n",
-    "robot_misses_turn": "Робот пропускает ход!\n",
-    "robot_use_homing_missiles": "Робот использует самонаводящиеся ракеты!\n",
-    "robot_use_regular_cartridges": "Робот использует патроны!\n",
-    "robot_throw_poison_grenade": "Робот бросил отравляющую гранату!\n",
-    "robot_jammed": "Робот заклинил!\n"
+    "robot_took_damage": "HIT! HIT! HIT!\nThe robot received {} units damage!\n",
+    "robot_health_info": "The robot's remaining health is {} units.\n",
+    "robot_missed": "The robot missed!\n",
+    "robot_misses_turn": "The robot skips a turn!\n",
+    "robot_use_homing_missiles": "The robot uses homing missiles!\n",
+    "robot_use_regular_cartridges": "The robot uses regular ammunition!\n",
+    "robot_throw_poison_grenade": "The robot threw a poison grenade!\n",
+    "robot_jammed": "The robot is jammed!\n"
 }
 
 GAME_RESULTS_MESSAGE = f"{'-' * 15}GAME RESULTS{'-' * 15}\n"
-WIN_MESSAGE = "{} победил!!!"
+WIN_MESSAGE = "{} WON!!!"
--- steps/helpers/variables.py
+++ steps/helpers/variables.py
@@ -1,4 +1,4 @@
-HERO_CHARACTER_NAME = "Герой"
+HERO_CHARACTER_NAME = "Hero"
 HERO_WAS_INJURED_EVENT = "hero_took_damage"
 HERO_FINISHED_EVENT = "hero_health_info"
 HERO_MISSED_EVENT = "hero_missed"
@@ -11,7 +11,7 @@ HERO_INJECTED_ADRENALINE_EVENT = "hero_injected_adrenaline"
 ADRENALINE_QTY_INFO = "adrenaline_qty_info"
 ADRENALINE_ENDED = "adrenaline_ended"
 
-ROBOT_CHARACTER_NAME = "Робот"
+ROBOT_CHARACTER_NAME = "Robot"
 ROBOT_WAS_INJURED_EVENT = "robot_took_damage"
 ROBOT_FINISHED_EVENT = "robot_health_info"
 ROBOT_MISSED_EVENT = "robot_missed"
@@ -22,16 +22,16 @@ ROBOT_THROW_POISON_GRENADE = "robot_throw_poison_grenade"
 ROBOT_JAMMED_EVENT = "robot_jammed"
 
 robot_data = {
-    "hp": 1300,  # жизненная энергия, запас здоровья
-    "defence": 120,  # защита, броня
-    "gun": 300  # оружие
+    "hp": 1300,
+    "defence": 120,
+    "gun": 300
 }
 
 hero_data = {
     "hp": 2000,
     "defence": 100,
     "gun": 250,
-    "protective_field": 150,  # защитное поле
+    "protective_field": 150,
     "has_shield": False,
     "adrenaline": 1,
     "adrenaline_power": 500
