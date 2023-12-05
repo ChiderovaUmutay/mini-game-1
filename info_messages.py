f0b0b94057c97b8b6b65dc6b7e3b405bd026bf10
uma <chiderovauma@gmail.com>
2023-12-05 11:56:47 +0600

    Moved all informative messages to dictionary objects.

---
+++ info_messages.py
@@ -0,0 +1,22 @@
+hero_info_messages: dict = {
+    "hero_took_damage": "HIT! HIT! HIT!\nГерой получил {} ед. урона!\n",
+    "hero_health_info": "Остаток здоровья у героя составляет {} ед.\n",
+    "hero_missed": "Герой промазал!\n",
+    "hero_misses_turn": "Герой пропускает ход!\n",
+    "hero_attacks": "Герой атакует!\n",
+    "hero_defends_himself": "Герой защищается и активирует защитное поле, сила которого равна {}!\n",
+    "hero_deactivate_protected_field": "Герой снял защитное поле!\n",
+}
+
+robot_info_messages: dict = {
+    "robot_took_damage": "HIT! HIT! HIT!\nРобот получил {} ед. урона!\n",
+    "robot_health_info": "Остаток здоровья у робота составляет {} ед.\n",
+    "robot_missed": "Робот промазал!\n",
+    "robot_misses_turn": "Робот пропускает ход!\n",
+    "robot_use_homing_missiles": "Робот использовал самонаводящиеся ракеты!\n",
+    "robot_use_regular_cartridges": "Робот использовал патроны!\n",
+    "robot_jammed": "Робот заклинил!\n"
+}
+
+GAME_RESULTS_MESSAGE = f"{'-' * 15}GAME RESULTS{'-' * 15}\n"
+WIN_MESSAGE = "{} победил!!!"
