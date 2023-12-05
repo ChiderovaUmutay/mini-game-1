ea237111e8ff37cc6738c7436a8f4e44e98d60ba
uma <chiderovauma@gmail.com>
2023-12-05 17:57:40 +0600

    Added type hints to "display_character_info()" function.

--- steps/helpers/display_functions.py
+++ steps/helpers/display_functions.py
@@ -7,7 +7,7 @@ from helpers.variables import HERO_WAS_INJURED_EVENT, \
 def display_hero_info(event: str, args=None) -> None or str:
     if event == HERO_WAS_INJURED_EVENT:
         display_character_info(args,
-                               character_messages_dict=hero_info_messages,
+                               character_messages=hero_info_messages,
                                consts=[HERO_WAS_INJURED_EVENT, HERO_FINISHED_EVENT])
     elif event == HERO_FINISHED_EVENT:
         return hero_info_messages.get(HERO_FINISHED_EVENT).format(args)
@@ -17,7 +17,7 @@ def display_hero_info(event: str, args=None) -> None or str:
 def display_robot_info(event: str, args=None) -> None or str:
     if event == ROBOT_WAS_INJURED_EVENT:
         display_character_info(args,
-                               character_messages_dict=robot_info_messages,
+                               character_messages=robot_info_messages,
                                consts=[ROBOT_WAS_INJURED_EVENT, ROBOT_FINISHED_EVENT])
     elif event == ROBOT_FINISHED_EVENT:
         robot_hp = args
@@ -25,9 +25,9 @@ def display_robot_info(event: str, args=None) -> None or str:
     else:
         print(robot_info_messages.get(event))
 
-def display_character_info(args, character_messages_dict, consts):
+def display_character_info(args: list, character_messages: dict, consts: list):
     dmg, hp = args[0], args[1]
     damage_const, health_const = consts[0], consts[1]
-    damage_info_message = character_messages_dict.get(damage_const).format(dmg)
-    health_info_message = character_messages_dict.get(health_const).format(hp)
+    damage_info_message = character_messages.get(damage_const).format(dmg)
+    health_info_message = character_messages.get(health_const).format(hp)
     print(f"{damage_info_message}{health_info_message}")
