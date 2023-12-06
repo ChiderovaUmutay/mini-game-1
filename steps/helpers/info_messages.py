2c82c12c3f7415258580355c7903dc22f53f6f33
uma <chiderovauma@gmail.com>
2023-12-06 14:28:37 +0600

    Changed the text of the messages a little and also added emoji to them.

--- steps/helpers/info_messages.py
+++ steps/helpers/info_messages.py
@@ -1,31 +1,31 @@
 hero_info_messages: dict = {
-    "hero_took_damage": "HIT! HIT! HIT!\nThe hero received {} units damage!\n",
-    "hero_health_info": "The hero's remaining health is {} units.\n",
-    "hero_missed": "The hero missed!\n",
-    "hero_misses_turn": "The hero misses a turn!\n",
-    "hero_attacks": "Hero attacks!\n",
-    "hero_defends_himself": "The hero defends himself and activates a protective field, the strength of which is equal to {}!\n",
-    "hero_deactivate_protected_field": "The hero removed the protective field!\n",
-    "hero_repelled_attack": "The hero repelled the attack with a protective field!\n",
-    "hero_injected_adrenaline": "The hero injected adrenaline!\n",
-    "adrenaline_qty_info": "The number of syringes with adrenaline in the backpack is equal to {}!\n",
-    "adrenaline_ended_info": "The syringes with adrenaline have run out!\nThe hero makes a second move.\n"
+    "hero_attacks": "👽🚀 ACTION: Hero attacks!\n",
+    "hero_defends_himself": "👽🚀 ACTION: The hero defends himself and activates a protective field, the strength of which is equal to {}!\n",
+    "hero_injected_adrenaline": "👽🚀 ACTION: The hero injected adrenaline!\n",
+    "hero_misses_turn": "👽🚀 ACTION: The hero misses a turn!\n",
+    "hero_took_damage": "🤖📈 RESULT: HIT💥 HIT💥 HIT💥\n👽📌 INFO: The hero received {} units damage!\n",
+    "hero_missed": "👽📈 RESULT: The hero missed!\n",
+    "hero_repelled_attack": "👽📈 RESULT: The hero repelled the attack with a protective field!\n",
+    "hero_deactivate_protected_field": "👽📌 INFO: The hero removed the protective field!\n",
+    "adrenaline_qty_info": "👽📌 INFO: The number of syringes with adrenaline in the backpack is equal to {}!\n",
+    "adrenaline_ended_info": "👽📌 INFO: The syringes with adrenaline have run out!\nThe hero makes a second move.\n",
+    "hero_health_info": "👽💊 HEALTH INFO: The hero's remaining health is {} units.\n",
 }
 
 robot_info_messages: dict = {
-    "robot_took_damage": "HIT! HIT! HIT!\nThe robot received {} units damage!\n",
-    "robot_health_info": "The robot's remaining health is {} units.\n",
-    "robot_missed": "The robot missed!\n",
-    "robot_misses_turn": "The robot skips a turn!\n",
-    "robot_use_homing_missiles": "The robot uses homing missiles!\n",
-    "robot_use_regular_cartridges": "The robot uses regular ammunition!\n",
-    "robot_throw_poison_grenade": "The robot threw a poison grenade!\n",
-    "robot_jammed": "The robot is jammed!\n"
+    "robot_misses_turn": "🤖🚀 ACTION: The robot skips a turn!\n",
+    "robot_use_homing_missiles": "🤖🚀 ACTION: The robot uses homing missiles!\n",
+    "robot_use_regular_cartridges": "🤖🚀 ACTION: The robot uses regular ammunition!\n",
+    "robot_throw_poison_grenade": "🤖🚀 ACTION: The robot threw a poison grenade!\n",
+    "robot_jammed": "🤖🚀 ACTION: The robot is jammed!\n",
+    "robot_missed": "🤖📈 RESULT: The robot missed!\n",
+    "robot_took_damage": "👽📈 RESULT: HIT💥 HIT💥 HIT💥\n🤖📌 INFO: The robot received {} units damage!\n",
+    "robot_health_info": "🤖💊 HEALTH INFO: The robot's remaining health is {} units.\n",
 }
 
 WELCOME_MESSAGE = f"{'-' * 30}👽 🆚 🤖{'-' * 30}\n"
 FAREWELL_MESSAGE = "\nThe game has stopped.\nGood luck👋🏻"
-INPUT_MESSAGE = "Enter one of actions ({}):\n"
-REPEAT_INPUT_MESSAGE = "Please enter an action from the list provided."
-GAME_RESULTS_MESSAGE = f"{'-' * 15}GAME RESULTS{'-' * 15}\n"
-WIN_MESSAGE = "{} WON!!!"
+INPUT_MESSAGE = "🕹 Enter one of actions ({}):\n"
+REPEAT_INPUT_MESSAGE = "🙋🏻 Please enter an action from the list provided."
+GAME_RESULTS_MESSAGE = f"{'-' * 23}GAME RESULTS{'-' * 23}\n"
+WIN_MESSAGE = f"{'-' * 24}" + "{} WON🏅" + f"{'-' * 24}\n"
